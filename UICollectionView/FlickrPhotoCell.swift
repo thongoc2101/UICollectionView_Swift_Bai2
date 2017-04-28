edc9f6b211937743c0266664508a358d8f655673
Phan Thi Ngoc Cam <camphan@Phans-Mac.local>
2017-04-29 00:07:03 +0700

    Initial Commit

---
+++ UICollectionView/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  UICollectionView
+//
+//  Created by Cntt05 on 4/26/17.
+//  Copyright © 2017 Cntt05. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Cntt05 on 4/26/17.
+//  Copyright © 2017 Cntt05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
